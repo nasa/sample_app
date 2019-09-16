9a175980bd04a4c862bcf48fbeeace4bfc595759
Jake Hageman <jacob.hageman@nasa.gov>
2019-09-16 16:12:35 -0400

    Update version

--- fsw/src/sample_app_version.h
+++ fsw/src/sample_app_version.h
@@ -31,7 +31,7 @@
 #define _sample_app_version_h_
 
 #define SAMPLE_APP_MAJOR_VERSION    1
-#define SAMPLE_APP_MINOR_VERSION    0
+#define SAMPLE_APP_MINOR_VERSION    1
 #define SAMPLE_APP_REVISION         0
 #define SAMPLE_APP_MISSION_REV      0
       
